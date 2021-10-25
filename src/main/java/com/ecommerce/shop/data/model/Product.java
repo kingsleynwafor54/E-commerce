0a4e214fee16a37519c07d62789a3e888771a318
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-10-25 15:11:52 +0100

    display product implementation

--- src/main/java/com/ecommerce/shop/data/model/Product.java
+++ src/main/java/com/ecommerce/shop/data/model/Product.java
@@ -1,6 +1,7 @@
 package com.ecommerce.shop.data.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.List;
@@ -12,17 +13,24 @@ public class Product {
             strategy = GenerationType.AUTO
     )
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     @Column
     private Double price;
+
     @Column
     @Enumerated(EnumType.STRING)
     private Currency currency;
+
     @Column(length = 500)
     private String details;
-    @ElementCollection
+
+    @ElementCollection(fetch= FetchType.EAGER)
+    @ToString.Exclude
     private List<String> imageUrl;
-    @OneToMany
+    
+    @OneToMany(fetch = FetchType.EAGER)
     private List<Feedback>feedbacks;
 }
--- src/main/java/com/ecommerce/shop/web/controller/HomeController.java
+++ src/main/java/com/ecommerce/shop/web/controller/HomeController.java
@@ -1,13 +1,49 @@
 package com.ecommerce.shop.web.controller;
 
+import com.ecommerce.shop.data.model.Product;
+import com.ecommerce.shop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    ProductService productServiceImpl;
+
     @GetMapping("/say-hello")
     public @ResponseBody String getWelcomeMessage(){
         return "Welcome to my first spring application";
+    }
+
+    @GetMapping("/index1")
+    public String getIndex(Model model){
+        List<Product> productList= productServiceImpl.findAll();
+        model.addAllAttributes(productList);
+        return "index1";
+    }
+    @GetMapping("/about")
+    public String getAbout(){
+        return "about";
+    }
+    @GetMapping("/product1")
+    public String getProduct(){
+        return "product1";
+    }
+    @GetMapping("/services")
+    public String getService(){
+        return "services";
+    }
+    @GetMapping("/single")
+    public String getSingle(){
+        return "single";
+    }
+    @GetMapping("/contact")
+    public String getContact(){
+        return "contact";
     }
 }
